3fe975efccd10ad13f679ed19cd0417829655416
Jenifer <85919892+jenifer98k@users.noreply.github.com>
2025-02-09 18:30:22 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,4 @@
-import bycrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 import transporter from "../config/nodemailer.js";
